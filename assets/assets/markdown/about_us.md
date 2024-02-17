a523335df972ba7f8f220e61929cb154390b2bfe
kcierpisz <26062599+Kamil3001@users.noreply.github.com>
2024-02-17 18:23:10 +0000

    First Commit

---
+++ assets/assets/markdown/about_us.md
@@ -0,0 +1,44 @@
+# About Us / Mission Statement
+
+American Football Ireland is the National Governing Body for the sport of American Football in Ireland and is responsible for all regulatory, competition, performance and development aspects of the game.
+
+#### MISSION STATEMENT
+
+American Football Ireland’s mission, as the national governing body, is to promote and teach all aspects of American Football in a fun and positive environment while adhering to our core values.
+
+#### CORE VALUES
+
+***Integrity***  
+We as an association aim to run the association with the utmost integrity, honesty and transparency at all times, through consistent and fair decision making and application of American Football Ireland’s By-laws and constitution. We, as an association, are accountable to our membership.
+
+***Respect***  
+We, as an association, aim to ensure respect between fellow team-mates, coaches, clubs, officials and administration and support them in all and any endeavours. We believe in fair play and creating a safe and fun environment throughout the association.
+
+***Inclusivity***  
+We, as an association, aim to create a diverse membership that doesn’t discriminate against individuals or clubs. We aim to provide an inclusive and family friendly environment for all.
+
+***Excellence***  
+We, as an association, aim to strive for excellence through constant review and development of all aspects of the association. We will set, work towards and aim to continuously achieve the highest of standards to inspire participation and further evolve the sport in the country at both club and national level.
+
+***Teamwork***  
+We, as an association, aim to commit to working together to promote and combine the skills of all members in a coordinated manner to assist and develop the association at all levels and to help us reach the association’s goals more efficiently. We will rely on each other and understand each individual has a part to play.
+
+#### ACTIVITIES
+
+American Football Ireland‘s activities are many and varied and include the following:
+- The organisation and promotion of all amateur domestic national competitions across both contact and non contact versions of the game.
+- The organisation and promotion of the national teams in international competition.
+- The promotion of the game to people of all ages, backgrounds and abilities and the provision of opportunities for their engagement and development.
+- The regulation of the game on and off the field of play through oversight of its rules.
+- The representation of the Irish game to national and international partners, be they sporting organisations, federations or commercial interests.
+
+American Football Ireland is recognised by [Sport Ireland] and the [Federation of Irish Sports] and is in the process of becoming an associate member of the International Federation of American Football ([IFAF]).
+
+#### MANAGEMENT & OPERATIONS
+
+American Football Ireland has a board and a staffing structure comprised of directors and officers who support the delivery of the sport across a series of directorates. You can find the list of board members, directors and supporting officers [here](https://www.americanfootball.ie/about-2/management-team/)
+
+[//]: #
+   [Sport Ireland]: <https://www.sportireland.ie/>
+   [Federation of Irish Sports]: <https://www.irishsport.ie/>
+   [IFAF]: <https://www.ifaf.org/>
---
+++ flutter_service_worker.js
@@ -0,0 +1,223 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+
+const RESOURCES = {"assets/AssetManifest.json": "8f6a2930d8a722cf7a1323649d59d4b6",
+"assets/AssetManifest.smcbin": "a89645da50a3a0226584d5ed2dff667d",
+"assets/assets/images/admirals_icon.png": "08685363029f1abcfe2eecd45d8acb41",
+"assets/assets/images/cowboys_icon.png": "00184dcb7ad6784b661a5a53b15fb2b2",
+"assets/assets/images/crusaders_icon.png": "0fb9051b41262d545f917a693b8dbe28",
+"assets/assets/images/eagles_icon.png": "2c05512c372bc45903b5b939bcbf757c",
+"assets/assets/images/giants_icon.png": "9b30e512b52890fa425a4954d3ed7e8c",
+"assets/assets/images/jets_icon.png": "90cb0a404cda48d4cb3f3c757aac52b0",
+"assets/assets/images/knights_icon.png": "c51b8c2642afb4fca960ca06a4c5079a",
+"assets/assets/images/mavericks_icon.png": "8af19aa4a12971b664094c6a96aad43d",
+"assets/assets/images/minotaurs_icon.png": "7e2dbbe613c7652a5868a016d607358b",
+"assets/assets/images/panthers_icon.png": "d7c162006b9e2679f5453d5dfd8770b2",
+"assets/assets/images/pirates_icon.png": "2fb85084230c7b4efbf31356b71ee13d",
+"assets/assets/images/razorbacks_icon.png": "b30baed9b903803109cf3716fc113e95",
+"assets/assets/images/rebels_icon.png": "d1d662a7dd58589aa4963ac23c21ca73",
+"assets/assets/images/rhinos_icon.png": "00d39caf0ed0d3c69bde46410aedf2de",
+"assets/assets/images/trinity_icon.png": "606b4ff5867f709339e009df44d2472e",
+"assets/assets/images/trojans_icon.png": "b8c7d0ae1381ffe3ccc38734b06ffe51",
+"assets/assets/images/ucd_icon.png": "b09f92457888e4b31e219663087069c9",
+"assets/assets/images/vikings_icon.png": "8e0785453533d92740819524843ca288",
+"assets/assets/images/vipers_icon.png": "9ecc100504cb08ec41e46e2544909ba1",
+"assets/assets/markdown/about_us.md": "b187377e8adb9f30db64bf025ab2b869",
+"assets/assets/markdown/the_game.md": "0d29c1ab743b9911237613e50c62655d",
+"assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
+"assets/fonts/MaterialIcons-Regular.otf": "dd72143253b694eb87429a89a99e980f",
+"assets/NOTICES": "432c4f35f5815ca6171e7f06e1a5c81c",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "57d849d738900cfd590e9adc7e208250",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "b00363533ebe0bfdb95f3694d7647f6d",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "0a94bab8e306520dc6ae14c2573972ad",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "9cda082bd7cc5642096b56fa8db15b45",
+"assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
+"canvaskit/canvaskit.js": "76f7d822f42397160c5dfc69cbc9b2de",
+"canvaskit/canvaskit.wasm": "f48eaf57cada79163ec6dec7929486ea",
+"canvaskit/chromium/canvaskit.js": "8c8392ce4a4364cbb240aa09b5652e05",
+"canvaskit/chromium/canvaskit.wasm": "fc18c3010856029414b70cae1afc5cd9",
+"canvaskit/skwasm.js": "1df4d741f441fa1a4d10530ced463ef8",
+"canvaskit/skwasm.wasm": "6711032e17bf49924b2b001cef0d3ea3",
+"canvaskit/skwasm.worker.js": "19659053a277272607529ef87acf9d8a",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"flutter.js": "6b515e434cea20006b3ef1726d2c8894",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"index.html": "c04e2f7f16a4d9ac95f70a116549536c",
+"/": "c04e2f7f16a4d9ac95f70a116549536c",
+"main.dart.js": "ba9f994c5ccb955b3455037d8beab463",
+"manifest.json": "7c2a7894435da2f53bc98be72cc9e1eb",
+"version.json": "fc9645dd4bfa97b5c13d087246648e56"};
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = ["main.dart.js",
+"index.html",
+"assets/AssetManifest.json",
+"assets/FontManifest.json"];
+
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache only if the resource was successfully fetched.
+        return response || fetch(event.request).then((response) => {
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      })
+    })
+  );
+});
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
