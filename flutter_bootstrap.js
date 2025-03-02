29fc9160c1acec2b75961ae9800f8269b6cc685b
Kamil3001 <kamil.cierpisz@ucdconnect.ie>
2025-03-02 14:09:49 +0000

    homepage updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "796231134"
+    serviceWorkerVersion: "1854125668"
   }
 });
